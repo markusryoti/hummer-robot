e05ac8637ef75c54d4eb3aea207fc15559a14169
Markus Ryöti <markus.ryoti@student.tut.fi>
2021-08-09 19:55:24 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Robo
+This is 4-wheel robot
+
+## Install dependencies for project
+sudo apt install avr-libc avrdude binutils-avr gcc-avr
+
---
+++ led.c
@@ -0,0 +1,16 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main()
+{
+	DDRB |= (1 << DDB5);
+
+	while (1)
+	{
+		PORTB |= (1 << PORTB5);
+		_delay_ms(250);
+
+		PORTB &= ~(1 << PORTB5);
+		_delay_ms(250);
+	}
+}
